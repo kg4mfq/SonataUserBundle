4f4193c806e6b4bdc7897e20a54629fc7ef1519e
Brian Crawford <brian@brian-crawford.com>
2015-01-11 21:17:17 -0600

    Remove deprecated use of forms and form handler

--- Controller/ProfileFOSUser1Controller.php
+++ Controller/ProfileFOSUser1Controller.php
@@ -11,6 +11,7 @@
 
 namespace Sonata\UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,7 @@ use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\GetResponseUserEvent;
 
 /**
  * This class is inspired from the FOS Profile Controller, except :
@@ -50,25 +52,39 @@ class ProfileFOSUser1Controller extends Controller
      *
      * @throws AccessDeniedException
      */
-    public function editAuthenticationAction()
+    public function editAuthenticationAction(Request $request)
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-
-        $form = $this->container->get('sonata.user.authentication.form');
-        $formHandler = $this->container->get('sonata.user.authentication.form_handler');
-
-        $process = $formHandler->process($user);
-        if ($process) {
-            $this->setFlash('sonata_user_success', 'profile.flash.updated');
-
-            return new RedirectResponse($this->generateUrl('sonata_user_profile_show'));
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
         }
-
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.profile.form.factory');
+        $form = $formFactory->createForm();
+        $form->setData($user);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+            $userManager = $this->get('fos_user.user_manager');
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
+            $userManager->updateUser($user);
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_profile_show');
+                $response = new RedirectResponse($url);
+            }
+            $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+            return $response;
+        }
         return $this->render('SonataUserBundle:Profile:edit_authentication.html.twig', array(
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ));
     }
 
