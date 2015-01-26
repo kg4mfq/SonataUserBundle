84fc62a5bcc3f5f656dc50d86632ff4d708b6831
kg4mfq <kg4mfq@users.noreply.github.com>
2015-01-26 10:19:25 -0600

    Update UserManager.php

--- Document/UserManager.php
+++ Document/UserManager.php
@@ -11,7 +11,7 @@
 
 namespace Sonata\UserBundle\Document;
 
-use FOS\UserBundle\Document\UserManager as BaseUserManager;
+use FOS\UserBundle\Model\UserManager as BaseUserManager;
 use Sonata\UserBundle\Model\UserManagerInterface;
 
 /**
